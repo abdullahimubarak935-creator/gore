13676f13d967a9b2b4bbf5221ef6ad7ebac1a346
Andre Renaud <andre@ignavus.net>
2025-06-23 20:48:35 +1200

    Helper for running the tests a lot

--- run_tests.sh
+++ run_tests.sh
@@ -2,11 +2,18 @@
 # We're not clearing out the state properly on `Run` at the moment, so all the
 # tests need to be in a separate process
 
-set -ex
+set -e
 
 if [ -n "$1" ] ; then
 	echo "Running tests in $1"
-	go test -v -count 1 -run "^${1}\$" .
+	COUNT=1
+	if [ -n "$2" ] ; then
+		COUNT=$2
+	fi
+	echo "Running Test $1 $COUNT times"
+	for i in $(seq $COUNT) ; do
+		go test -v -count 1 -run "^${1}\$" .
+	done
 else
 	TEST=$(go test -test.list '.*' | grep '^Test')
 	echo "All tests: $TEST"
